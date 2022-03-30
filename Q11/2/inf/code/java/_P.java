a4e56f42d81077b9578b44376b111e0422ceb17a
AKB1152 <mcpe9005@gmail.com>
2022-03-30 12:59:57 +0000

    correct gitignore

--- Q11/2/inf/code/java/_P.java
+++ Q11/2/inf/code/java/_P.java
@@ -1,30 +1,44 @@
-import java.awt.Graphics;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
 
 import javax.swing.JFrame;
 
 
 public class _P extends JFrame{
 
-	private int z;
+	private int _z;
 
-	public _P () {
+	public _P () throws IOException {
+		File f = new File ("../../../../ignore/log_P.log");
+		f.createNewFile();
+		PrintStream ps = new PrintStream (f, "utf-8");
+		System.setErr(ps);
 		setSize(900, 900);
 		setDefaultCloseOperation(3);
 		setName("×-—·—-×");
-		z = 0; 
+		setVisible(true);
+		_z = 0; 
 	}
 
 	@Override
 	public void paint(Graphics g) {
+		double _x=0, _y;
+		System.err.printf("Color values:\n z |    x    |    y    \n———|—————————|—————————\n");
 		for (int x = 0; x < getWidth(); x++) {
 			for (int y = 0; y < getHeight(); y++) {
-				g.setColor(new Color (((z%256)*0x10000)+((x%256)*0x100)+((y%256))));
+				_z %= 256;
+				_x = x * (256/getWidth()*0x100);
+				_y = y * (256/getWidth());
+				g.setColor(new Color (((_z%256)*0x10000)+(int)(_x+_y)%0x1000000));
+				System.err.printf ("%2x | %3.4f | %3.4f \n", _z, _x, _y);
 				g.drawLine(x, y, x, y);
 			}
 		}
 
-		z++;	
+		_z++;	
 		try {
 			Thread.sleep(125);
 		} catch (Exception e) {
@@ -32,7 +46,7 @@ public class _P extends JFrame{
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		_P __ = new _P(); 
 	
 		while (true) __.repaint();
